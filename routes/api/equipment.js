dbec5e029b07c569dd51c042a14feedcf17f80ea
Jose Castro <jacastro023@gmail.com>
2020-02-25 10:32:01 -0600

    added routes

---
+++ routes/api/equipment.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const equipmentController = require("../../controllers/equipmentControllers.js");
+
+// Matches with "/api/equipment"
+router.route("/")
+  .get(equipmentController.findAll)
+  .post(equipmentController.create);
+
+// Matches with "/api/equipment/:id"
+router
+  .route("/:id")
+  .get(equipmentController.findById)
+  .put(equipmentController.update)
+  .delete(equipmentController.remove);
+
+module.exports = router;
